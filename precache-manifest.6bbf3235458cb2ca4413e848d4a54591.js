f8ad0ff67d988e1547dfe98880c730bc4cbbb3c3
nalinikanta123 <nalinikantam@gmail.com>
2020-04-11 19:41:44 -0400

    Updates

---
+++ precache-manifest.6bbf3235458cb2ca4413e848d4a54591.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebe6afcedf5f9904e2a7fd5548868e33",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e2b17ac7c12cc2499ca5",
+    "url": "/monsters-rolodex/static/css/main.8f6c94d1.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2b17ac7c12cc2499ca5",
+    "url": "/monsters-rolodex/static/js/main.1c348caf.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
